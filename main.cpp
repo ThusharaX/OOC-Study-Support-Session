44985094fc00de60657997c606508041ceccae49
Thushara Thiwanka <47711719+ThusharaX@users.noreply.github.com>
2021-10-18 18:28:22 +0000

    Static Dynamic Added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 
+using namespace std;
+
+class Student {
+	private:
+		string name;
+	public:
+		void setName(string n);
+		void printName();
+};
+
+void Student::setName(string n) {
+	name = n;
+}
+
+void Student::printName() {
+	cout << "Name : " << name << endl;
+}
+
 int main() {
 	
-	std::cout << "Hello World!\n";
+	// Static
+	Student s1;
+	s1.setName("Thushara");
+	s1.printName();
+
+	// Dynamic
+	Student *s2 = new Student();
+	s2->setName("Thiwanka");
+	s2->printName();
+	delete s2;
 
 	return 0;
 }
