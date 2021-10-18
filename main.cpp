1411ca6a93a0945116f0b13335c303cd504c40c2
Thushara Thiwanka <47711719+ThusharaX@users.noreply.github.com>
2021-10-18 17:14:08 +0000

    Formatting Output Added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 int main() {
 	
-	std::cout << "Hello World!\n";
+	cout<< setw(12) <<setiosflags(ios::fixed) << 25.695789<< endl;
+	cout<< setw(12) <<setprecision (3) << 25.695789<< endl;
+	cout<< setw(12) << setprecision (5) << 25.695789 << endl;
 
 	return 0;
 }
