53ff7284b4ecb992343369872c6f4b3b02e9e676
Thushara Thiwanka <47711719+ThusharaX@users.noreply.github.com>
2021-10-19 14:00:54 +0000

    Setters & Getters

--- main.cpp
+++ main.cpp
@@ -2,9 +2,25 @@
 
 using namespace std;
 
+class Student {
+	private:
+		string name;
+		int age;
+	public:
+		void setName(string n) {
+			name = n;
+		}
+		string getName() {
+			return name;
+		}
+};
+
 int main() {
 	
-	cout << "Hello World!\n";
+	Student s;
+
+	s.setName("Thushara");
+	cout << s.getName() << endl;
 
 	return 0;
 }
