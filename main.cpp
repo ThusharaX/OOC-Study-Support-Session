41c9e62cc72cf57f8d70d3c7a2d53952ba463e6b
Thushara Thiwanka <47711719+ThusharaX@users.noreply.github.com>
2021-10-19 17:09:51 +0000

    Update

--- main.cpp
+++ main.cpp
@@ -56,174 +56,174 @@ using namespace std;
 
 // Bi-directional Association
 
-// #define SIZE 10
+#define SIZE 10
 
-// class Customer;
-// class Order;
+class Customer;
+class Order;
 
-// class Customer
-// {
-// private:
-// 	string name;
-// 	string address;
-// 	Order *order[SIZE];
-// 	int noOfOrders;
+class Customer
+{
+private:
+	string name;
+	string address;
+	Order *order[SIZE];
+	int noOfOrders;
 
-// public:
-// 	Customer();
-// 	Customer(string pname, string paddress);
-// 	void addOrder(Order *O);
-// 	void displayCustomer();
-// };
+public:
+	Customer();
+	Customer(string pname, string paddress);
+	void addOrder(Order *O);
+	void displayCustomer();
+};
 
-// class Order
-// {
-// private:
-//    	string orderID;
-//    	Customer *Cus;
+class Order
+{
+private:
+   	string orderID;
+   	Customer *Cus;
    
-// public:  
-//    	Order(string	porderID,  Customer *pCus);
-//    	void displayOrders();
-// };
+public:  
+   	Order(string	porderID,  Customer *pCus);
+   	void displayOrders();
+};
 
-// Order::Order (string porderID,  Customer *pCus) 
-// {  
-//     orderID = porderID;
-//     Cus = pCus;
-//     Cus->addOrder(this);
-// }
+Order::Order (string porderID,  Customer *pCus) 
+{  
+    orderID = porderID;
+    Cus = pCus;
+    Cus->addOrder(this);
+}
 
-// void Order::displayOrders()
-// {
-// 	cout << "\t\tOrder ID : " << orderID << endl;
-// }
+void Order::displayOrders()
+{
+	cout << "\t\tOrder ID : " << orderID << endl;
+}
 
-// Customer::Customer(string pname, string paddress)
-// {
-//     name = pname;
-//     address = paddress;
-//     noOfOrders = 0;
-// }
+Customer::Customer(string pname, string paddress)
+{
+    name = pname;
+    address = paddress;
+    noOfOrders = 0;
+}
 
-// void Customer::addOrder(Order *O)
-// {
-//     if (noOfOrders < SIZE) {
-//     	order[noOfOrders] = O;
-// 	}
-//     noOfOrders++;
-// }
+void Customer::addOrder(Order *O)
+{
+    if (noOfOrders < SIZE) {
+    	order[noOfOrders] = O;
+	}
+    noOfOrders++;
+}
 
-// void Customer::displayCustomer()
-// {
-//     cout << "\tCustomer Name  = " << name << endl;
-//     cout << "\tCustomer Address  = " << address << endl;
-//     for( int i = 0; i < noOfOrders; i++) {
-//         order[i]-> displayOrders();
-// 	}
-// }
+void Customer::displayCustomer()
+{
+    cout << "\tCustomer Name  = " << name << endl;
+    cout << "\tCustomer Address  = " << address << endl;
+    for( int i = 0; i < noOfOrders; i++) {
+        order[i]-> displayOrders();
+	}
+}
 
 
 
-// int main()
-// {
-// 	Customer *C1 = new Customer("Thushara", "Kegalle");
-// 	Customer *C2 = new Customer("Ashen" , "Veyangoda");
+int main()
+{
+	Customer *C1 = new Customer("Thushara", "Kegalle");
+	Customer *C2 = new Customer("Ashen" , "Veyangoda");
 
-// 	Order *O1 = new Order("001", C1);
-// 	Order *O2 = new Order("002",  C2);
-// 	Order *O3 = new Order("003",  C2);
+	Order *O1 = new Order("001", C1);
+	Order *O2 = new Order("002",  C2);
+	Order *O3 = new Order("003",  C2);
 
-// 	cout << "\nOrders of C1" << endl;
-// 	C1->displayCustomer();
+	cout << "\nOrders of C1" << endl;
+	C1->displayCustomer();
 	
-// 	cout << "\nOrders of C2" << endl;
-// 	C2->displayCustomer();
+	cout << "\nOrders of C2" << endl;
+	C2->displayCustomer();
 
-// 	return 0;
-// }
+	return 0;
+}
 
 
 //----------------------------------------------------------
 // Association Class 
 
-class Employee
-{
-private:
-	string empID;
-	string name;
-public:
-	Employee(string pempID, string pname)
-	{
-		empID = pempID;
-		name = pname;
-	}
+// class Employee
+// {
+// private:
+// 	string empID;
+// 	string name;
+// public:
+// 	Employee(string pempID, string pname)
+// 	{
+// 		empID = pempID;
+// 		name = pname;
+// 	}
 	
-	void displayEmployee()
-	{
-		cout << "Emp ID = " << empID << endl;
-		cout << "Name = " << name << endl;
-	}
-};
+// 	void displayEmployee()
+// 	{
+// 		cout << "Emp ID = " << empID << endl;
+// 		cout << "Name = " << name << endl;
+// 	}
+// };
 
-class Project
-{
-private:
-	string projectID;
-	string name;
-public:
-	Project(string pprojectID, string pname)
-	{
-		projectID = pprojectID;
-		name = pname;
-	}
+// class Project
+// {
+// private:
+// 	string projectID;
+// 	string name;
+// public:
+// 	Project(string pprojectID, string pname)
+// 	{
+// 		projectID = pprojectID;
+// 		name = pname;
+// 	}
 
-	void displayProject()
-	{
-		cout << "Project ID = " << projectID << endl;
-		cout << "Name  = " << name << endl;
-	}
-};
+// 	void displayProject()
+// 	{
+// 		cout << "Project ID = " << projectID << endl;
+// 		cout << "Name  = " << name << endl;
+// 	}
+// };
 
-class worksON
-{
-private:
-	Employee *emp;
-	Project *prj;
-	int hours;
-public:
-	worksON(Employee *pemp, Project *pprj, int phours)
-	{
-		emp = pemp;
-		prj = pprj;
-		hours = phours;
-	}
-	void displayWorksON()
-	{
-		emp->displayEmployee();
-		prj->displayProject();
-		cout << "Hours = " << hours << endl;
-	}
-};
+// class worksON
+// {
+// private:
+// 	Employee *emp;
+// 	Project *prj;
+// 	int hours;
+// public:
+// 	worksON(Employee *pemp, Project *pprj, int phours)
+// 	{
+// 		emp = pemp;
+// 		prj = pprj;
+// 		hours = phours;
+// 	}
+// 	void displayWorksON()
+// 	{
+// 		emp->displayEmployee();
+// 		prj->displayProject();
+// 		cout << "Hours = " << hours << endl;
+// 	}
+// };
 
-int main()
-{
-	Employee *e[2];
-	e[0] = new Employee("E001", "Thushara");
-	e[1] = new Employee("E002", "Ashen");
+// int main()
+// {
+// 	Employee *e[2];
+// 	e[0] = new Employee("E001", "Thushara");
+// 	e[1] = new Employee("E002", "Ashen");
 	
-	Project *p[2];
-	p[0] = new Project("P001", "Python");
-	p[1] = new Project("P002", "Java");
+// 	Project *p[2];
+// 	p[0] = new Project("P001", "Python");
+// 	p[1] = new Project("P002", "Java");
 	
-	worksON work1(e[0], p[0], 2);
-	worksON work2(e[1], p[1], 4);
+// 	worksON work1(e[0], p[0], 2);
+// 	worksON work2(e[1], p[1], 4);
 	
-	work1.displayWorksON();
-	cout << endl;
-  	work2.displayWorksON();
+// 	work1.displayWorksON();
+// 	cout << endl;
+//   	work2.displayWorksON();
 	
-	delete e[0];
+// 	delete e[0];
   
-	return 0;
-}
+// 	return 0;
+// }
