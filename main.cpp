a8dfbf512c6ec4513643f31e0a303d1a9c03eeea
Thushara Thiwanka <47711719+ThusharaX@users.noreply.github.com>
2021-10-19 15:40:53 +0000

    this

--- main.cpp
+++ main.cpp
@@ -5,9 +5,15 @@ using namespace std;
 class Student {
 	private:
 		string name;
+		string getName() {
+			return name;
+		}
 	public:
 		void setName(string n);
 		void printName();
+		void print() {
+			cout << this->name << endl;
+		}
 };
 
 void Student::setName(string n) {
@@ -21,14 +27,15 @@ void Student::printName() {
 int main() {
 	
 	// Static
-	Student s1;
-	s1.setName("Thushara");
-	s1.printName();
+	// Student s1;
+	// s1.setName("Thushara");
+	// s1.printName();
 
 	// Dynamic
 	Student *s2 = new Student();
 	s2->setName("Thiwanka");
-	s2->printName();
+	// s2->printName();
+	s2->print();
 	delete s2;
 
 	return 0;
