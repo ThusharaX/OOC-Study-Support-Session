e067353246c5041c8e15722175f661d2ea8f4c59
Thushara Thiwanka <47711719+ThusharaX@users.noreply.github.com>
2022-05-29 11:41:33 +0000

    Update

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@ int main() {
 	
 	cout<< setw(12) <<setiosflags(ios::fixed) << 25.695789<< endl;
 	cout<< setw(12) <<setprecision (3) << 25.695789<< endl;
-	cout<< setw(12) << setprecision (5) << 25.695789 << endl;
+	cout<< setw(12) <<setprecision (5) << 25.695789 << endl;
+
+	cout << "123456789012" << endl;
 
 	return 0;
 }
