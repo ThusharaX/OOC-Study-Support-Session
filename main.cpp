be5ec117994276ba3c68d9558b6b549f829a5aca
Thushara Thiwanka <47711719+ThusharaX@users.noreply.github.com>
2022-05-29 09:13:09 +0000

    Pass by Value Done

--- main.cpp
+++ main.cpp
@@ -2,9 +2,27 @@
 
 using namespace std;
 
+void func(int pX, int pY) {
+	pX = pX + 4;
+	pY = pY + 4;
+}
+
 int main() {
 	
-	cout << "Hello World!\n";
+	cout << "Pass By Value\n\n";
+
+	int x = 10;
+	int y = 5;
+
+	// Before
+	cout << "X: " << x << endl;
+	cout << "Y: " << y << endl;
+
+	func(x, y);
+
+	// After
+	cout << "X: " << x << endl;
+	cout << "Y: " << y << endl;
 
 	return 0;
 }
