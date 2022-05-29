7dec12ef24195390361fd53b6b71989907f774c3
Thushara Thiwanka <47711719+ThusharaX@users.noreply.github.com>
2022-05-29 10:16:39 +0000

    Overriding

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+class Animal {
+	private:
+		string name;
+	public:
+		void talk() {
+			cout << "I am " << name << endl;
+		}
+};
+
+
+
 int main() {
 	
 	cout << "Hello World!\n";
