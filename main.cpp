83649f0e994888eb0cc48aabc1af32d107d0e1c3
Thushara Thiwanka <47711719+ThusharaX@users.noreply.github.com>
2022-05-29 11:09:05 +0000

    Update

--- main.cpp
+++ main.cpp
@@ -2,21 +2,23 @@
 
 using namespace std;
 
-void foo(int num) {
-	num += 4;
+void foo(int *num) {
+	*num += 4;
 }
 
 int main() {
 	
-	int x = 5;		// 0x7ffd1da6e24c
-	int *y;
-	y = &x;
-	cout << *y << endl;
+	// int x = 5;		// 0x7ffc6ce212c4
+	// cout << &x << endl;
+	// int *y;
+	// y = &x;
+	
+	// cout << *y << endl;
 
-	// int num = 5;
-	// foo(num);
+	int num = 5;
+	foo(&num);
 
-	// cout << num << endl;
+	cout << num << endl;
 
 	return 0;
 }
