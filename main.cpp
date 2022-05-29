014735033cb7431eb350287a8a4c49bb98ad535b
Thushara Thiwanka <47711719+ThusharaX@users.noreply.github.com>
2022-05-29 09:35:42 +0000

    This Keyword Done

--- main.cpp
+++ main.cpp
@@ -2,9 +2,30 @@
 
 using namespace std;
 
+class Student {
+	private:
+		string name;
+		int age;
+
+	public:
+		Student(string name, int age) {
+			this->name = name;
+			this->age = age;
+		}
+
+		void print() {
+			cout << "Name: " << this->name << endl;
+			cout << "Age: " << this->age << endl;
+		}
+};
+
 int main() {
 	
-	cout << "Hello World!\n";
+	cout << "This Keyword\n\n";
+
+	Student std1("Thushara", 25);
+
+	std1.print();
 
 	return 0;
 }
