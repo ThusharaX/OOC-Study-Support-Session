dab6a6abe23b14a7b5e6ba0a4afec309c8109728
Thushara Thiwanka <47711719+ThusharaX@users.noreply.github.com>
2022-05-29 09:10:43 +0000

    Pass By Reference Done

--- main.cpp
+++ main.cpp
@@ -2,9 +2,25 @@
 
 using namespace std;
 
+int func(int *pX, int *pY) {
+	*pX = *pX + 4;
+	*pY = *pY + 4;
+}
+
 int main() {
 	
-	cout << "Hello World!\n";
+	cout << "Pass By Reference\n\n";
+
+	int x = 10;
+	int y = 5;
+
+	cout << "X: " << x << endl;
+	cout << "Y: " << y << endl;
+
+	func(&x, &y);
+
+	cout << "X: " << x << endl;
+	cout << "Y: " << y << endl;
 
 	return 0;
 }
