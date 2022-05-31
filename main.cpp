9bbe9f86d515ddfa7ee3d02b7d9062212d8e034f
Thushara Thiwanka <47711719+ThusharaX@users.noreply.github.com>
2022-05-31 05:07:04 +0000

    2019-June-o2-(b)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
+//Picture class - parent class
+class Picture {
+	protected:
+		string title;
+		double price;
 	
-	cout << "2019-June-02-(b)\n";
+	public:
+		void updatePrice();
+};
 
-	return 0;
-}
+//Photograph class - child
+class Photograph: public Picture {
+	private:
+		string photograph;
+		string camera;
+		int speed;
+		int aperture;
+	
+	public:
+		void alterContrast();
+};
+
+//Painting class - child
+class Painting: public Picture {
+	private:
+		string artist;
+		string type;
+		string owner;
+	
+	public:
+		void printprovenance();
+};
